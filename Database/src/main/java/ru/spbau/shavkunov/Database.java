ed0022395bd2be84b7237a07ec58bfdac448b60f
shavkunov <mv.shavkunov@yandex.ru>
2016-12-04 13:11:32 +0300

    minor fix

--- Database/src/main/java/ru/spbau/shavkunov/Database.java
+++ Database/src/main/java/ru/spbau/shavkunov/Database.java
@@ -7,19 +7,16 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Database {
+    /**
+     * Необходимые поля для работы с БД.
+     */
     private static Statement stmt;
+    private static Connection c;
 
+    /**
+     * Основная логика программы.
+     */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        File db = new File("content.db");
-        if (db.exists()) {
-            db.delete();
-        }
-
-        Connection c = DriverManager.getConnection("jdbc:sqlite:content.db");
-        c.createStatement().execute("PRAGMA foreign_keys = ON");
-        c.setAutoCommit(false);
-        stmt = c.createStatement();
-
         initDatabase();
 
         Scanner input = new Scanner(System.in);
@@ -54,7 +51,7 @@ public class Database {
 
                     System.out.println("Ваши результаты:");
                     while (results.next()) {
-                        System.out.println(results.getInt(results.findColumn("number")));
+                        System.out.println(results.getInt("number"));
                     }
 
                     break;
@@ -63,11 +60,12 @@ public class Database {
                 case 3: {
                     System.out.println("Введите телефон для поиска");
                     long number = input.nextLong();
+                    input.nextLine();
                     ResultSet results = getNames(number);
 
                     System.out.println("Ваши результаты:");
                     while (results.next()) {
-                        System.out.println(results.getString(results.findColumn("name")));
+                        System.out.println(results.getString("name"));
                     }
 
                     break;
@@ -89,6 +87,7 @@ public class Database {
                     String name = input.nextLine();
                     System.out.println("Введите номер телефона только цифрами");
                     long number = input.nextLong();
+                    input.nextLine();
                     System.out.println("Введите новое имя пользователя телефоном");
                     String newName = input.nextLine();
                     changeName(name, newName, number);
@@ -101,8 +100,10 @@ public class Database {
                     String name = input.nextLine();
                     System.out.println("Введите номер телефона только цифрами");
                     long number = input.nextLong();
+                    input.nextLine();
                     System.out.println("Введите новый номер телефона только цифрами");
                     long newNumber = input.nextLong();
+                    input.nextLine();
                     changeNumber(name, number, newNumber);
 
                     break;
@@ -115,7 +116,7 @@ public class Database {
 
                     ResultSet results = stmt.executeQuery(queryAllEntries);
                     while(results.next()) {
-                        System.out.println(results.findColumn("name") + " " + results.findColumn("number"));
+                        System.out.println(results.getString("name") + " " + results.getLong("number"));
                     }
 
                     break;
@@ -131,11 +132,17 @@ public class Database {
         c.close();
     }
 
+    /**
+     * Изменение номера в паре имя - телефон.
+     * @param name имя.
+     * @param number старый телефонный номер.
+     * @param newNumber новый телефонный номер.
+     */
     private static void changeNumber(String name, long number, long newNumber) throws SQLException {
         Integer nameIndex = checkName(name);
         Integer numberIndex = checkNumber(number);
 
-        String querуCountNamesOfNumber = "SELECT COUNT(*) FROM Name_to_number WHERE number_ID = " + numberIndex;
+        String querуCountNamesOfNumber = "SELECT COUNT(*) AS total FROM Name_to_number WHERE number_ID = " + numberIndex;
         if (stmt.executeQuery(querуCountNamesOfNumber).getInt("total") == 1) {
             String queryUpdateNumber = "UPDATE Number SET number = " + newNumber + " WHERE ID = " + numberIndex;
             stmt.executeUpdate(queryUpdateNumber);
@@ -143,7 +150,7 @@ public class Database {
             String queryInsertNewNumber = "INSERT INTO Numbers VALUES (" + newNumber + ")";
             stmt.executeUpdate(queryInsertNewNumber);
             String queryGettingNewIndex = "SELECT ID From Numbers WHERE number = " + newNumber;
-            int newIndex = stmt.executeQuery(queryGettingNewIndex).findColumn("ID");
+            int newIndex = stmt.executeQuery(queryGettingNewIndex).getInt("ID");
             String queryUpdateRelation = "UPDATE Name_to_number SET number_ID = " + newIndex +
                                          " WHERE name_ID = " + nameIndex + " AND number_ID = " + numberIndex;
 
@@ -151,19 +158,26 @@ public class Database {
         }
     }
 
+    /**
+     * Изменение имени в паре имя - телефон.
+     * @param name старое имя.
+     * @param newName новое имя
+     * @param number телефонны номер.
+     * @throws SQLException
+     */
     private static void changeName(String name, String newName, long number) throws SQLException {
         Integer nameIndex = checkName(name);
         Integer numberIndex = checkNumber(number);
 
-        String querуCountNumbersOfName = "SELECT COUNT(*) FROM Name_to_number WHERE name_ID = " + nameIndex;
+        String querуCountNumbersOfName = "SELECT COUNT(*) AS total FROM Name_to_number WHERE name_ID = " + nameIndex;
         if (stmt.executeQuery(querуCountNumbersOfName).getInt("total") == 1) {
             String queryUpdateName = "UPDATE Names SET name = '" + newName + "' WHERE ID = " + nameIndex;
             stmt.executeUpdate(queryUpdateName);
         } else {
-            String queryInsertNewName = "INSERT INTO Names VALUES ('" + newName + "')";
-            stmt.executeQuery(queryInsertNewName);
+            String queryInsertNewName = "INSERT INTO Names(name) VALUES ('" + newName + "')";
+            stmt.executeUpdate(queryInsertNewName);
             String queryGettingNewIndex = "SELECT ID From Names WHERE name = '" + newName + "'";
-            int newIndex = stmt.executeQuery(queryGettingNewIndex).findColumn("ID");
+            int newIndex = stmt.executeQuery(queryGettingNewIndex).getInt("ID");
             String queryUpdateRelation = "UPDATE Name_to_number SET name_ID = " + newIndex +
                                          " WHERE name_ID = " + nameIndex + " AND number_ID = " + numberIndex;
 
@@ -171,6 +185,9 @@ public class Database {
         }
     }
 
+    /**
+     * Удаление записи имени и телефона из БД
+     */
     private static void deleteEntry(String name, long number) throws SQLException {
         Integer nameIndex = checkName(name);
         Integer numberIndex = checkNumber(number);
@@ -179,24 +196,27 @@ public class Database {
             return;
         }
 
-        String queryDeleteRelation = "DELETE name_ID, number_ID FROM Name_to_number " +
+        String queryDeleteRelation = "DELETE FROM Name_to_number " +
                                      "WHERE name_ID = " + nameIndex + " AND number_ID = " + numberIndex;
 
-        stmt.executeQuery(queryDeleteRelation);
+        stmt.executeUpdate(queryDeleteRelation);
 
-        String queryDidNameHaveNumber = "SELECT COUNT(*) FROM Name_to_number WHERE name_ID = " + nameIndex;
+        String queryDidNameHaveNumber = "SELECT COUNT(*) AS total FROM Name_to_number WHERE name_ID = " + nameIndex;
         if (stmt.executeQuery(queryDidNameHaveNumber).getInt("total") == 0) {
-            String queryDeleteName = "DELETE ID, name FROM Names WHERE name = '" + name + "'";
-            stmt.executeQuery(queryDeleteName);
+            String queryDeleteName = "DELETE FROM Names WHERE name = '" + name + "'";
+            stmt.executeUpdate(queryDeleteName);
         }
 
-        String queryDidNumberHaveName = "SELECT COUNT(*) FROM Name_to_number WHERE number_ID = " + numberIndex;
+        String queryDidNumberHaveName = "SELECT COUNT(*) AS total FROM Name_to_number WHERE number_ID = " + numberIndex;
         if (stmt.executeQuery(queryDidNumberHaveName).getInt("total") == 0) {
-            String queryDeleteNumber = "DELETE ID, number FROM Numbers WHERE number = " + number;
-            stmt.executeQuery(queryDeleteNumber);
+            String queryDeleteNumber = "DELETE FROM Numbers WHERE number = " + number;
+            stmt.executeUpdate(queryDeleteNumber);
         }
     }
 
+    /**
+     * Получение курсора всех номеров по заданному имени.
+     */
     private static ResultSet getNumbers(String name) throws SQLException {
         String queryFindNumbersByName = "SELECT number from Numbers WHERE ID = " +
                                         "(SELECT number_ID FROM Name_to_number " +
@@ -204,6 +224,9 @@ public class Database {
         return stmt.executeQuery(queryFindNumbersByName);
     }
 
+    /**
+     * Получение курсора всех имен по заданному номеру.
+     */
     private static ResultSet getNames(long number) throws SQLException {
         String queryFindNamesByNumber = "SELECT name from Names WHERE ID = " +
                                         "(SELECT name_ID FROM Name_to_number " +
@@ -211,39 +234,56 @@ public class Database {
         return stmt.executeQuery(queryFindNamesByNumber);
     }
 
+    /**
+     * Проверка на существование имени в таблице имен.
+     * @param name существование этого имени хотим проверить.
+     * @return null, если имени не существует, иначе индекс его строки в БД.
+     */
     @Nullable
     private static Integer checkName(String name) throws SQLException {
-        String queryCheckName = "SELECT EXISTS(SELECT ID FROM Names WHERE name = '" + name + "')";
+        String queryCheckName = "SELECT ID FROM Names WHERE name = '" + name + "'";
         ResultSet cursor = stmt.executeQuery(queryCheckName);
-        if (!cursor.next()) {
-            return cursor.getInt(cursor.findColumn("ID"));
+        if (cursor.next()) {
+            return cursor.getInt("ID");
         }
 
         return null;
     }
 
+    /**
+     * Проверка на существование номера в таблице номеров.
+     * @param number существование этого номера хотим проверить.
+     * @return null, если номера не существует, иначе индекс его строки в БД.
+     */
     @Nullable
     private static Integer checkNumber(Long number) throws SQLException {
         String queryCheckNumber = "SELECT ID FROM Numbers WHERE number = " + number;
         ResultSet cursor = stmt.executeQuery(queryCheckNumber);
         if (cursor.next()) {
-            return cursor.getInt(cursor.findColumn("ID"));
+            return cursor.getInt("ID");
         }
 
         return null;
     }
 
+    /**
+     * Добавление в БД пары имени и телефона.
+     */
     private static void addEntry(String name, long number) throws SQLException {
         Integer nameIndex = checkName(name);
         if (nameIndex == null) {
             String queryAddName = "INSERT INTO Names(name) VALUES ('" + name + "')";
-            nameIndex = stmt.executeUpdate(queryAddName);
+            stmt.executeUpdate(queryAddName);
+            String findNewIndex = "SELECT ID FROM Names WHERE name = '" + name + "'";
+            nameIndex = stmt.executeQuery(findNewIndex).getInt("ID");
         }
 
         Integer numberIndex = checkNumber(number);
         if (numberIndex == null) {
             String queryAddNumber = "INSERT INTO Numbers(number) VALUES (" + number + ")";
-            numberIndex = stmt.executeUpdate(queryAddNumber);
+            stmt.executeUpdate(queryAddNumber);
+            String findNewIndex = "SELECT ID FROM Numbers WHERE number = " + number;
+            numberIndex = stmt.executeQuery(findNewIndex).getInt("ID");
         }
 
         String queryCheckRelation = "SELECT name_ID, number_ID FROM Name_to_number " +
@@ -251,7 +291,6 @@ public class Database {
                                     " AND number_ID = " + numberIndex;
 
         ResultSet cursor = stmt.executeQuery(queryCheckRelation);
-
         if (cursor.next()) {
             System.out.println("Такая запись уже существует!");
         } else {
@@ -262,7 +301,21 @@ public class Database {
         }
     }
 
+    /**
+     * Инициализация БД. Создание трех пустых таблиц. Название БД - content.db.
+     */
     private static void initDatabase() throws SQLException {
+        File db = new File("content.db");
+        if (db.exists()) {
+            db.delete();
+        }
+
+
+        c = DriverManager.getConnection("jdbc:sqlite:content.db");
+        c.createStatement().execute("PRAGMA foreign_keys = ON");
+        c.setAutoCommit(false);
+        stmt = c.createStatement();
+
         String queryCreateTableNumbers = "CREATE TABLE Numbers (" +
                                          "ID INTEGER PRIMARY KEY NOT NULL, " +
                                          "number INTEGER NOT NULL)";
